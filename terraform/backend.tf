f8e4ce317006e4062e5419069d3aee2c6a0d4599
Dave James Miller <dave@davejamesmiller.com>
2018-12-01 11:56:19 +0000

    Remove Terraform

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-# Store state on S3 so it can be shared between different computers
-terraform {
-  backend "s3" {
-    bucket  = "davejamesmiller-terraform"
-    key     = "collector/terraform.tfstate"
-    region  = "eu-west-1"
-  }
-}
