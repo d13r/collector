badbaf9ec535da265428ffc28751104e690eb587
Dave James Miller <dave@davejamesmiller.com>
2019-01-13 23:29:53 +0000

    Add ?m= parameter to pre-fill message

--- public/index.php
+++ public/index.php
@@ -22,7 +22,7 @@
 
                 <!-- Message -->
                 <div class="message-row">
-                    <textarea name="message" class="message" autofocus></textarea>
+                    <textarea name="message" class="message" autofocus><?= e($_GET['m'] ?? '') ?></textarea>
                 </div>
 
                 <div class="buttons-row">
