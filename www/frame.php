0ed7559b207c5ee0734f58200ece8f5be53eee66
Dave James Miller <dave@davejamesmiller.com>
2020-06-02 08:45:24 +0100

    Disable spell check

--- www/frame.php
+++ www/frame.php
@@ -23,7 +23,7 @@
 
                 <!-- Message -->
                 <div class="message-row">
-                    <textarea name="message" class="message" autofocus><?= e($_GET['m'] ?? '') ?></textarea>
+                    <textarea name="message" class="message" autofocus spellcheck="false"><?= e($_GET['m'] ?? '') ?></textarea>
                 </div>
 
                 <div class="buttons-row">
