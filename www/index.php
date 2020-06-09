89e5b91fb25d57dceb3aa055da89b33cb7d04ea2
Dave James Miller <dave@davejamesmiller.com>
2020-06-09 23:31:20 +0100

    Switch back to a single input area to help me focus on one thing at a time
    Also increased the font size because I don't want to be writing a lot
    and what I do write should be in my face

--- www/index.php
+++ www/index.php
@@ -9,22 +9,75 @@
 
         <title>Whiteboard</title>
 
-        <link rel="stylesheet" href="<?= asset_url('frames.css') ?>">
+        <link rel="stylesheet" href="<?= asset_url('main.css') ?>">
         <link rel="icon" href="<?= asset_url('whiteboard.png') ?>">
         <link rel="manifest" href="<?= asset_url('manifest.json') ?>">
 
     </head>
     <body>
-        <div class="container">
-            <div class="column" style="flex: 2;">
-                <iframe src="/frame.php?target=<?= urlencode($_GET['target'] ?? '') ?>&amp;m=<?= urlencode($_GET['m'] ?? '') ?>"
-                    onload="this.contentDocument.querySelector('[autofocus]').focus()"></iframe>
+
+        <?php if (logged_in()): ?>
+
+            <!-- Main page -->
+            <form class="form">
+
+                <!-- Message -->
+                <div class="message-row">
+                    <textarea name="message" class="message" autofocus spellcheck="false"><?= e($_GET['m'] ?? '') ?></textarea>
+                </div>
+
+                <div class="buttons-row">
+
+                    <!-- Target -->
+                    <?php if (count($config['targets']) > 1): ?>
+                        <div class="targets">
+                            <?php foreach ($config['targets'] as $id => $target): ?>
+                                <label class="target">
+                                    <input
+                                        type="radio"
+                                        name="target"
+                                        value="<?= e($id) ?>"
+                                        accesskey="<?= e($target['shortcut']) ?>"
+                                        tabindex="-1"
+                                        <?= checked(is_current_target($id)) ?>
+                                    >
+                                    <span class="target-text"><?= e($target['title']) ?></span>
+                                </label>
+                            <?php endforeach ?>
+                        </div>
+                    <?php else: ?>
+                        <input type="hidden" name="target" value="<?= e(current_target()) ?>">
+                    <?php endif ?>
+
+                    <!-- Send -->
+                    <button class="submit button" tabindex="-1">Send</button>
+
+                </div>
+
+                <!-- Confirmation -->
+                <div class="confirmation">Message Sent</div>
+
+            </form>
+            <script src="<?= asset_url('main.js') ?>"></script>
+
+        <?php else: ?>
+
+            <!-- Login -->
+            <div class="login-container">
+                <form class="login" action="/login.php" method="post">
+                    <div class="login-field">
+                        <input name="username" type="text" placeholder="Username" autofocus>
+                    </div>
+                    <div class="login-field">
+                        <input name="password" type="password" placeholder="Password">
+                    </div>
+                    <div class="login-button">
+                        <button type="submit">Log In</button>
+                    </div>
+                </form>
             </div>
-            <div class="column">
-                <iframe src="/frame.php?target=<?= urlencode($_GET['target'] ?? '') ?>&amp;m=<?= urlencode("") ?>"></iframe>
-                <iframe src="/frame.php?target=<?= urlencode($_GET['target'] ?? '') ?>&amp;m=<?= urlencode("") ?>"></iframe>
-                <iframe src="/frame.php?target=<?= urlencode($_GET['target'] ?? '') ?>&amp;m=<?= urlencode("") ?>"></iframe>
-            </div>
-        </div>
+
+        <?php endif ?>
+
     </body>
 </html>
