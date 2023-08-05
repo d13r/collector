0da82b783bdb063d4b7955ac4e3f7877d071f918
Dave James Miller <d@djm.me>
2023-08-05 17:41:32 +0100

    Update example config file

--- config/collector.example.php
+++ config/collector.example.php
@@ -15,9 +15,13 @@ return [
             'title' => 'Home',
             'shortcut' => 'h',
             'return' => 'your@email.com',
-            'from' => '"Your name" <your@email.com>',
-            'replyto' => ['"Your name" <your@email.com>'],
-            'to' => ['"Your name" <your@email.com>'],
+            'from' => ['your@email.com', 'Your name'],
+            'replyto' => [
+                ['your@email.com', 'Your name'],
+            ],
+            'to' => [
+                ['your@email.com', 'Your name'],
+            ],
             'color' => '#00f',
         ],
 
@@ -25,9 +29,13 @@ return [
             'title' => 'Work',
             'shortcut' => 'w',
             'return' => 'your@email.com',
-            'from' => '"Your name" <your@email.com>',
-            'replyto' => ['"Your name" <your@work.com>'],
-            'to' => ['"Your name" <your@work.com>'],
+            'from' => ['your@email.com', 'Your name'],
+            'replyto' => [
+                ['Your name', 'your@work.com'],
+            ],
+            'to' => [
+                ['Your name', 'your@work.com'],
+            ],
             'color' => '#080',
         ],
 
@@ -35,9 +43,15 @@ return [
             'title' => 'Both',
             'shortcut' => 'b',
             'return' => 'your@email.com',
-            'from' => '"Your name" <your@email.com>',
-            'replyto' => ['"Your name" <your@email.com>', '"Your name" <your@work.com>'],
-            'to' => ['"Your name" <your@email.com>', '"Your name" <your@work.com>'],
+            'from' => ['your@email.com', 'Your name'],
+            'replyto' => [
+                ['your@email.com', 'Your name'],
+                ['Your name', 'your@work.com'],
+            ],
+            'to' => [
+                ['your@email.com', 'Your name'],
+                ['Your name', 'your@work.com'],
+            ],
             'color' => '#c00',
         ],
 
